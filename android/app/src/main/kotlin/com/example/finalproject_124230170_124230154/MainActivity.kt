0501398d8b48a42d292236cfdae83b9227b9d0cf
tugas1 <acel021204@gmail.com>
2025-11-11 19:58:32 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/finalproject_124230170_124230154/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.finalproject_124230170_124230154
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/config/api_amadeus_config.dart
@@ -0,0 +1,5 @@
+class ApiConfig {
+  static const String baseUrl = "https://test.api.amadeus.com";
+  static const String apiKey = "eDVG98zvBDyaI3rz4NGdGRBbOxLRJFcl";
+  static const String apiSecret = "ELPvuGShxMJKXzfq";
+}
